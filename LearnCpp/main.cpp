63ce69e1901b889127f69c997cf70d80daa3d1ef
Роберт Роберт <Nadzar@yandex.ru>
2021-12-18 00:29:49 +0300

    newQuest

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
-	std::cout << "Hello World and Git" << std::endl;
-	return 0;  
+	int x = 5, y = 5;
+	std::cout << x << " " << y << std::endl;
+	std::cout << ++x << " " << --y << std::endl; // версия префикс
+	std::cout << x << " " << y << std::endl;
+	std::cout << x++ << " " << y-- << std::endl; // версия постфикс
+	std::cout << x << " " << y << std::endl;
+
+	return 0;
 }
