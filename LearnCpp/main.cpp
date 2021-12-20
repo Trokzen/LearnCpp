c7020722d8356f172f9006be772592559d05acce
Роберт Роберт <tms64xx@yandex.ru>
2021-12-20 11:38:03 +0300

    new

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -4,9 +4,9 @@ int main()
 {
 	int x = 5, y = 5;
 	std::cout << x << " " << y << std::endl;
-	std::cout << ++x << " " << --y << std::endl; // âåðñèÿ ïðåôèêñ
+	std::cout << ++x << " " << --y << std::endl;
 	std::cout << x << " " << y << std::endl;
-	std::cout << x++ << " " << y-- << std::endl; // âåðñèÿ ïîñòôèêñ
+	std::cout << x++ << " " << y-- << std::endl;
 	std::cout << x << " " << y << std::endl;
 
 	return 0; 
