71d0fe840e0768138acd4450f043ebdede206a04
Роберт Роберт <tms64xx@yandex.ru>
2021-12-20 11:50:57 +0300

    ss

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-	int x = 5, y = 5;
-	std::cout << x << " " << y << std::endl;
+	int x = 5, y = 5, z=5;
+	std::cout << x << " " << y << std::endl;//dsds
 	std::cout << ++x << " " << --y << std::endl;
 	std::cout << x << " " << y << std::endl;
 	std::cout << x++ << " " << y-- << std::endl;
