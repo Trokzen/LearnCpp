6bafa121f87c65b42c86bd35f8a68a13e8701ee7
Robert JAR <tms64xx@yandex.ru>
2023-03-24 09:24:16 +0300

    change coment

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	int x = 5, y = 5, z=5;
-	std::cout << x << " " << y << std::endl;//dsds
+	std::cout << x << " " << y << std::endl;//example coment
 	std::cout << ++x << " " << --y << std::endl;
 	std::cout << x << " " << y << std::endl;
 	std::cout << x++ << " " << y-- << std::endl;
