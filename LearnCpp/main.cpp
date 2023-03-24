4bd05166f748289c86a428eafa9a1efe5cf36387
Robert JAR <tms64xx@yandex.ru>
2023-03-24 14:05:22 +0300

    void change

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -8,6 +8,6 @@ int main()
 	std::cout << x << " " << y << std::endl;
 	std::cout << x++ << " " << y-- << std::endl;
 	std::cout << x << " " << y << std::endl;  
-	z = 25;//bug fixed
+	z = 25;//bug fixed new
 	return 0; 
 }
