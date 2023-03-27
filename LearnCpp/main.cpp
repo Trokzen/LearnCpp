fed6927e66615ce52698a2743e20cf831a5b0254
Robert JAR <tms64xx@yandex.ru>
2023-03-27 17:20:57 +0300

    change other line

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -8,6 +8,6 @@ int main()
 	std::cout << x << " " << y << std::endl;
 	std::cout << x++ << " " << y-- << std::endl;
 	std::cout << x << " " << y << std::endl;  
-	z = 25;//bug fixed new
+	z = 25;//bug fixed new and change line 
 	return 0; 
 }
